26cfba5f9b260162191a0cce907a2c54431db907
lcy21 <3446228498@qq.com>
2024-11-26 17:13:59 +0800

    Update denglu.js

--- denglu.js
+++ denglu.js
@@ -1,7 +1,7 @@
 $('#login-button').click(function (event) {
     let userName = document.getElementById("userName").value;
     let pwd = document.getElementById("pwd").value;
-    if (userName == "追吻辰星" && pwd == "lcy") {
+    if (userName == "lcy" && pwd == "lcy") {
         
         event.preventDefault();
         $('form').fadeOut(2500);
